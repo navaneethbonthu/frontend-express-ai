f666bde6bb068215f7f24101cca67f6ca0ce8d49
Mahesh <chaitanyadarlapudi@gmail.com>
2026-02-16 13:24:06 +0530

    updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,11 @@ import { authGuard } from './components/auth/auth.guard';
 import { publicGuard } from './components/auth/public.guard';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'home',
+    loadComponent: () => import('./components/home/home').then((m) => m.Home),
+  },
   {
     path: 'products',
     canActivate: [authGuard],
